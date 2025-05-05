9536014dbf3cdb5b3fbf5481e6fdc98a433caed7
Cody Green <codyjeromegreen@gmail.com>
2025-05-05 11:29:38 -0700

    solution

--- binary_tree_lab.py
+++ binary_tree_lab.py
@@ -6,10 +6,23 @@ class TreeNode:
         self.left: Optional['TreeNode'] = None
         self.right: Optional['TreeNode'] = None
 
-# TODO: Implement the max_depth function
+# Solution to find the maximum depth of a binary tree
 def max_depth(root: Optional[TreeNode]) -> int:
-    pass
+    if root is None:
+        return 0
+    # Recursively find the max depth of left and right subtrees
+    left_depth = max_depth(root.left)
+    right_depth = max_depth(root.right)
+    # Add 1 for the current node
+    return 1 + max(left_depth, right_depth)
 
-# TODO: Implement the lowest_common_ancestor function
+# Solution to find the lowest common ancestor in a Binary Search Tree
 def lowest_common_ancestor(root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
-    pass
+    # Use BST properties to move left or right
+    if p.val < root.val and q.val < root.val:
+        return lowest_common_ancestor(root.left, p, q)
+    elif p.val > root.val and q.val > root.val:
+        return lowest_common_ancestor(root.right, p, q)
+    else:
+        # If p and q are on opposite sides or equal to root, root is LCA
+        return root
